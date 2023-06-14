30daf1d2f13887f483c3d01a07cf255aa32077cb
SP RAKSHITH <rakshith0908@gmail.com>
2023-06-14 20:57:56 +0200

    Created SocketIO for Real Time communication

---
+++ socket_app.py
@@ -0,0 +1,38 @@
+import os
+import time
+import eventlet
+from flask import Flask
+from flask_socketio import SocketIO
+
+eventlet.monkey_patch()
+
+app = Flask(__name__)
+socketio = SocketIO(app, async_mode='eventlet', cors_allowed_origins="*")
+
+
+def background_thread():
+    while True:
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        file_path = os.path.join(dir_path, "LatestWeatherData/latest_weather_data.txt")
+
+        with open(file_path, 'r') as file:
+            latest_message = file.read()
+            socketio.emit('LATEST_WEATHER_REPORT', latest_message)
+            print("Emitted Data...!")
+
+        time.sleep(120)
+
+
+@socketio.on('connect')
+def test_connect():
+    print('Client connected')
+    socketio.start_background_task(background_thread)
+
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='127.0.0.1', port=12345)
